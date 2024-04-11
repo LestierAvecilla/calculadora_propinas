01e61862e1ca0019fb28c344c74e77bbf9ca1938
LestierAvecilla <lestier.avecilla@my.unitec.edu.mx>
2024-04-11 12:23:51 -0600

    Añadir dependencia @midudev/tailwind-animations en package.json

--- src/components/MenuItems.tsx
+++ src/components/MenuItems.tsx
@@ -1,5 +1,6 @@
 import type { MenuItem } from "../types"
 
+
 type MenuItemProps = {
     item:MenuItem,
     addItem: (item: MenuItem) => void 
@@ -10,7 +11,7 @@ type MenuItemProps = {
 export default function MenuItems({item, addItem}:MenuItemProps) {
   return (
     <button
-      className="border-2 border-teal-400 hover:bg-teal-200 p-3 flex justify-between w-full"
+      className="border-2 border-teal-400 hover:animate-pop animate-duration-normal animate-steps-modern p-3 flex justify-between w-full"
       onClick={() => addItem(item)}
     >
 
